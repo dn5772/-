a6ebf27a5c8ddd2aa3521d08f897d43515f20c17
dn5772 <64554030+dn5772@users.noreply.github.com>
2021-04-14 23:17:31 +0900

    Add files via upload

---
+++ opp3_4.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class opp3_4 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        char alp = 'a';
+        char alp_array[] = new char[26];
+
+        for (int i=0; i<26; i++){
+            alp_array[i] = alp;
+            alp++;
+        }
+
+        String s = scanner.next();
+        char c = s.charAt(0);
+        int a;
+
+        for (int i=0; i<26; i++){
+            if (c == alp_array[i]){
+                a = i;
+            }
+        }
+
+        for (int i=0; i<a; i++){
+            for ();
+            System.out.print();
+
+        }
+
+    }
+
+}
