bfc65dd22677c5b4314fab3c4d7b6ee34628a395
dn5772 <64554030+dn5772@users.noreply.github.com>
2021-05-13 23:54:34 +0900

    Update Calculator.cpp

--- Calculator.cpp
+++ Calculator.cpp
@@ -27,13 +27,13 @@ class Calculator
 
     public : 
     
-    int getErrorCode();                             // 오류코드 반환
+    int getErrorCode();                             
 
-    int setExpression(const char* expr);       // expr에 전달된 수식(중위표기식)을 postfix로 변경하고 계산하는 함수      
+    int setExpression(const char* expr);           
 
                                                             //    오류 없는경우, 0, 오류가 있는 경우, 1을 반환 
-    String getPostFix();                            // 변환된 후위표기식을 반환   --> 오류가 있을경우 최초값인 공백이 리턴
+    String getPostFix();                            
     int getValue();                                   // 수식 오류있음 --> 예외발생
-                                                      // 수식 오류없음 --> 결과값 리턴
+                                                     
 };
 
