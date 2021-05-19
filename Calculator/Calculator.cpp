5c04db928d91246be2e0fad070d76e2249fc5bf2
dn5772 <64554030+dn5772@users.noreply.github.com>
2021-05-19 14:42:54 +0900

    Update Calculator.cpp

--- Calculator/Calculator.cpp
+++ Calculator/Calculator.cpp
@@ -1,39 +1,45 @@
 #include "List.h"
 #include "String.h"
+#include "Stack.h"
+#include <iostream>
 
 using namespace std;
 
 class Calculator 
-
 {
     private : 
 
-    List<String> tokens; 
-    int errCode;                  // 발생된 오류코드 값 : 0 -> 오류 없음 , 다른 값 -> 오류 있음
-    int value;                     // 계산된 값
-    String postfix;             
-    int makePostFix();         
-    int a;
+    List<String> tokens;
+    int errCode;
+    int value;
+    String postfix;
 
-                                       // 오류 없는경우, 0, 오류가 있는 경우, 1을 반환 
-
-                                       // 변경결과는 postfix 변수에 저장, 오류시 적절한 코드를 errCode 에 저장 (오류코드는 각자가 정의)
-
-    int evaluation();            // postfix 를 계산하는 함수 : 
-
-                                       // 계산된 값을 구함, 오류 없는경우 0, 오류가 있는 경우, 1을 반환
-
-                                       // 계산된 값은 value 에 저장, 오류시 적절한 코드를 errCode 에 저장 (오류코드는 각자가 정의)
+    int makePostFix();
+    int evaluation();
 
     public : 
     
-    int getErrorCode();                             
+    Calculator();
+    ~Calculator();
+    int getErrorCode();
+    int setExpression(const char* expr);
+    String getPostFix();
+    int getValue();
 
-    int setExpression(const char* expr);           
-
-                                                            //    오류 없는경우, 0, 오류가 있는 경우, 1을 반환 
-    String getPostFix();                            
-    int getValue();                                   // 수식 오류있음 --> 예외발생
-                                                     
 };
 
+Calculator::Calculator(){
+    
+}
+
+int Calculator::makePostFix(){
+
+}
+
+int Calculator::evaluation(){
+
+}
+
+int Calculator::getErrorCode(){
+
+}
