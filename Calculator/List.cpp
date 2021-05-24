72b9a1a4433dfc1c847b88b74a219d562ea7e7e1
dn5772 <64554030+dn5772@users.noreply.github.com>
2021-05-24 23:37:53 +0900

    Update List.cpp

--- Calculator/List.cpp
+++ Calculator/List.cpp
@@ -5,7 +5,7 @@ template <typename T>
 class List {
     private :
 
-    int* items;
+    T* items;
     int itemCount;
     int size;
 
@@ -14,44 +14,22 @@ class List {
     List();
     ~List();
 
-    int getItem(int index);
-    void addItem(int itm);
-    void insertItem(int index, int itm);
+    T getItem(int index);
+    int getItemCount();
+    void addItem(T itm);
+    void insertItem(int index,T itm);
     int removeAt(int index);
-    int removeItem(int itm);
+    T removeItem(T itm);
     void concat(List& list);
     void print();
 
 };
 
-
-class OrderedList {
-    private :
-
-    int *items;
-    int itemCount;
-    int size;
-
-    public :
-
-    OrderedList();
-    ~OrderedList();
-
-    int getItem(int index);
-    void addItem(int itm);
-    int removeAt(int index);
-    int removeItem(int itm);
-    void concat(OrderedList& olist);
-    void print();
-
-};
-
-
 template <typename T> 
 List<T>::List(){
     itemCount = 0;
     size = 5;
-    items = new int[size];
+    items = new T[size];
 }
 
 template <typename T> 
@@ -60,19 +38,16 @@ List<T>::~List(){
 }
 
 template <typename T> 
-int List<T>::getItem(int index){
+T List<T>::getItem(int index){
     if (0 <= index && index < itemCount)
         return items[index];
-    else
-        return -99999;
 }
 
 template <typename T> 
-void List<T>::addItem(int itm){
+void List<T>::addItem(T itm){
     if (itemCount < size){
         items[itemCount] = itm;
         itemCount++;
-        //cout << itemCount;
     }
     else {
         int *newItems = new int[size*2];
@@ -80,16 +55,19 @@ void List<T>::addItem(int itm){
             newItems[i] = items[i];
         newItems[itemCount] = itm;
         itemCount++;
-        delete[] items;
+        delete[] items;1
         items = newItems;
         size = size * 2;
     }
 }
 
+template <typename T>
+int List<T>::getItemCount(){ return itemCount; }
+
 template <typename T> 
-void List<T>::insertItem(int index, int itm){
+void List<T>::insertItem(int index,T itm){
     if (itemCount == size){
-        int *newItems = new int[size*2];
+        T *newItems = new T[size*2];
         for (int i=0; i<itemCount; i++)
             newItems[i] = items[i];
         itemCount++;
@@ -123,10 +101,10 @@ int List<T>::removeAt(int index){
 }
 
 template <typename T> 
-int List<T>::removeItem(int itm){
+T List<T>::removeItem(T itm){
     for (int i=0; i<itemCount; i++){
         if (items[i]==itm){
-            int tmp = items[i];
+            T tmp = items[i];
             for (int j=i; j<itemCount; j++){
                 items[j] = items[j+1];
             }
@@ -134,7 +112,6 @@ int List<T>::removeItem(int itm){
             return tmp;
         }
     }
-    return -99999;
 }
 
 template <typename T> 
@@ -149,3 +126,74 @@ void List<T>::print(){
     for (int i=0; i<itemCount; i++)
         cout << items[i] << endl;
 }
+/*
+int main(){
+    char list, c;
+    bool loop = 1;
+
+    cout << "1 : List\n2 : OrderedList" << endl;
+
+    cin >> list;
+
+    List<int> list_a, list_b;
+
+    if (list == '1'){
+        while (loop){
+            int lindex=0, litm=0;
+            
+            cout << "1 : getItem \n2 : addItem \n3 : insertItem \n4 : removeAt \n5 : removeItem \n6 : concat \n7 : print \n8 : END"<< endl;
+
+            cin >> c;            
+
+            switch (c){
+                case '1' : 
+                cout << "index :";
+                cin >> lindex;
+                cout << list_a.getItem(lindex) << endl;
+                break;
+
+                case '2' :
+                cout << "itm :";
+                cin >> litm;
+                list_a.addItem(litm);
+                break;
+
+                case '3' :
+                cout << "index itm :";
+                cin >> lindex >> litm;
+                list_a.insertItem(lindex, litm);
+                break;
+
+                case '4' :
+                cout << "index :";
+                cin >> lindex;
+                cout << "REMOVE : " << list_a.removeAt(lindex) << endl;
+                break;
+
+                case '5' :
+                cout << "itm :";
+                cin >> litm;
+                cout << "REMOVE : " << list_a.removeItem(litm) << endl;
+                break;
+
+                case '6' :
+                for (int i=0; i<20; i++){
+                    list_b.addItem(i);
+                }
+                list_a.concat(list_b);
+                break;
+
+                case '7' :
+                list_a.print();
+                break;
+
+                case '8' :
+                loop = 0;
+                break;
+            }
+       }
+    }
+
+    return 0;
+}
+*/
