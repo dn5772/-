d6be6080eb69d21a09039f4f2135af85f8adc887
dn5772 <64554030+dn5772@users.noreply.github.com>
2021-07-02 23:50:50 +0900

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,92 @@
+import turtle as t
+import random
+
+class Ractangle :
+    length = None
+    color = []
+    x = None
+    y = None
+
+    def __init__(self):
+        self.length = 100
+
+        for i in range(3) :
+            c = random.randint(0, 255)
+            self.color.append(c)
+
+        self.x = random.randint(-300, 300)
+        self.y = random.randint(-300, 300)
+
+    def draw(self):
+
+        t.colormode(255)
+        t.penup()
+        t.goto(self.x, self.y)
+        t.pendown()
+        t.pencolor(self.color[0], self.color[1], self.color[2])
+
+        for i in range(4):
+            t.forward(self.length)
+            t.left(90)
+
+    def changeRact(self):
+        for i in range(3) :
+            c = random.randint(0, 255)
+            self.color[i] = c
+
+        self.x = random.randint(-300, 300)
+        self.y = random.randint(-300, 300)
+
+    def loop(self, a):
+        for i in range(a):
+            self.changeRact()
+            self.draw()
+
+class Triangle :
+    length = None
+    color = []
+    x = None
+    y = None
+
+    def __init__(self):
+        self.length = 60
+
+        for i in range(3) :
+            c = random.randint(0, 255)
+            self.color.append(c)
+
+        self.x = random.randint(0, 100)
+        self.y = random.randint(0, 100)
+
+
+    def draw(self):
+        t.colormode(255)
+        t.penup()
+        t.goto(self.x, self.y)
+        t.pendown()
+        t.pencolor(self.color[0], self.color[1], self.color[2])
+
+        for i in range(3):
+            t.forward(self.length)
+            t.left(120)
+
+    def changeRact(self):
+        for i in range(3) :
+            c = random.randint(0, 255)
+            self.color[i] = c
+
+        self.x = random.randint(-200, 200)
+        self.y = random.randint(-200, 200)
+
+    def loop(self, a):
+        for i in range(a):
+            self.changeRact()
+            self.draw()
+
+ract = None
+tri = None
+ract = Ractangle()
+tri = Triangle()
+
+tri.loop(10)
+ract.loop(10)
